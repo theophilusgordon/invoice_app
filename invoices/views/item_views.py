15ce72e86ed3e90a69b28514c2c73049ba2455ff
Theophilus Gordon <theophilus.gordon@amalitech.com>
2024-12-23 12:18:30 +0000

    feat: migrations

---
+++ invoices/views/item_views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+
+from ..models import Item
+from ..serializers import ItemSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save()
